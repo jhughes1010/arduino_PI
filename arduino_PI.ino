1ffaa36b1841b5a899f09380423b450a873838ea
James Hughes <jhughes1010@gmail.com>
2021-09-03 17:55:32 -0600

    pin assignments entered

---
+++ arduino_PI.ino
@@ -0,0 +1,22 @@
+//Arduino PI detector
+//Transcribed by James Hughes
+
+//Pin Assignments
+byte txPin = 8;
+byte mainSamplePin = 9;
+byte audioPin = 10;
+byte boostPin = 11;
+byte delayPin = A0;
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
