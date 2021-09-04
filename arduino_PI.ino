5649b0c868d0ba39a01ef9dfb33f884a1c6f0736
James Hughes <jhughes1010@gmail.com>
2021-09-03 18:12:33 -0600

    initial code entry WIP

--- arduino_PI.ino
+++ arduino_PI.ino
@@ -9,6 +9,34 @@ byte boostPin = 11;
 byte delayPin = A0;
 
 
+//Program Constants
+const float normalPower = 50e-6;
+const float boostPower = 100e-6;
+const float clockCycle = 62.5e-9;
+const unsigned long maxCount = 65535;
+const byte readDelayLimit = 100;
+const byte mosfetOn = HIGH;
+const byte mosfetOff = LOW;
+const byte syncDemodOn = LOW;
+const byte syncDemodOff = HIGH;
+
+
+//Detector timings
+float txOn = normalPower;
+float defMainDelay = normalPower;
+float mainDelay = defMainDelay;
+float mainSample = 50e-6;
+float efeDelay = 240e-6;
+float efeSample = mainSample;
+float txPeriod = 15e-3;
+
+
+//Timing offsets
+
+
+//Program variables
+
+
 
 
 void setup() {
