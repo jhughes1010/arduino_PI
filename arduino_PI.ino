5c927674bba853b23a82fd4bb1d520a719ce4187
James Hughes <jameshughes@Jamess-MacBook-Air.local>
2022-01-14 14:20:00 -0600

    moved variables to config.h

--- arduino_PI.ino
+++ arduino_PI.ino
@@ -15,68 +15,6 @@
 #ifdef LCD
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 #endif
-bool boost = false;
-
-//=================================
-// Pin assignments
-//=================================
-byte txPin = 8;          // Assign TX output
-byte mainSamplePin = 9;  // Assign main sample pulse
-byte efeSamplePin = 10;  // Assign EFE sample pulse
-byte audioPin = 11;      // Assign audio chopper
-byte boostPin = 12;      // Assign boost switch
-byte delayPin = A0;      // Assign delay pot
-byte batteryPin = A1;    // Assign battery monitor
-byte signalPin = A3;     // Assign signal monitor
-
-//=================================
-// Program constants
-//=================================
-const float normalPower = 50 US;       // Normal TX-on time (50us)
-const float boostPower = 100 US;       // Boost TX-on time (100us)
-const float clockCycle = 62.5 NS;      // Time for one clock cycle (1/16MHz)
-const unsigned long maxCount = 65535;  // Value of 2^16 - 1
-const byte readDelayLimit = 100;       // Wait 100 TX periods (100ms) before reading delay pot
-const byte mosfetOn = HIGH;            // Mosfet turns on when transmitter input high
-const byte mosfetOff = LOW;            // Mosfet turns off when transmitter input low
-const byte syncDemodOn = LOW;          // Sample gate turns on when input high
-const byte syncDemodOff = HIGH;        // Sample gate turns off when input low
-
-//=================================
-// Detector timings
-//=================================
-float txOn = normalPower;        // TX-on time using normal power mode
-float defMainDelay = 10 US;      // Default main sample delay (20us)
-float mainDelay = defMainDelay;  // Main sample pulse delay
-float mainSample = 50 US;        // Main sample pulse width (50us)
-float efeDelay = 240 US;         // EFE sample pulse delay (240us)
-float efeSample = mainSample;    // EFE sample pulse width (same as main sample)
-float txPeriod = 1 MS;           // TX period (1ms)
-
-//=================================
-// Timing offsets
-//=================================
-float txOnOffset = 3 US;         // TX-on pulse offset (3us)
-float mainDelayOffset = 4.2 US;  // Main delay pulse offset (4.2us)
-float mainSampleOffset = 3 US;   // Main sample pulse offset (3us)
-float efeDelayOffset = 12 US;    // EFE delay pulse offset (12us)
-float efeSampleOffset = 4 US;    // EFE sample pulse offset (4us)
-float txPeriodOffset = 30 US;    // TX period offset (30us)
-
-//=================================
-// Program variables
-//=================================
-word txOnCount;                                  // TX pulse
-word mainDelayCount;                             // Main sample delay
-word mainSampleCount;                            // Main sample pulse
-word efeDelayCount;                              // EFE sample delay
-word efeSampleCount;                             // EFE sample pulse
-word txPeriodCount;                              // TX period
-word delayVal = 0;                               // Delay pot value
-bool readDelayPot = false;                       // Delay pot read (true or false)
-byte audioLevel = LOW;                           // Value for audio chopper signal
-byte intState = 0;                               // Interrupt state machine
-byte readDelayCounter = 0;                       // Read delay pot counter
 
 //=================================
 //setup()
